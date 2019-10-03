ace666273808357c5f6a34a4e89bac3324b585d6
MrHan <1323232302@qq.com>
2019-10-03 13:40:56 +0800

    添加发布试卷

--- src/views/academic/child/read-upper-exam.vue
+++ src/views/academic/child/read-upper-exam.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-        <Drawer v-model="value" :width="800">
+        <Drawer v-model="value" :width="800" :closable="false">
+            <template slot="header">
+                <Button type="error" @click="handleEndExam(examData)">结束考试</Button>
+            </template>
             <Card :bordered="false" :dis-hover="true">
-
+                <Table :data="computed_table_data" :columns="columns" :border="true"></Table>
             </Card>
-            <Card :bordered="false" :dis-hover="true">
-                <Table :data="computed_table_data" :columns="columns" :border="true" :loading="loading">
+        </Drawer>
 
-                </Table>
+        <Drawer v-model="value" :closable="false" :width="800" placement="left" :mask="false">
+            <Card  style="background-color: #2c3e50;margin: 10px 0px 10px 0px;color: white;height: 400px" :bordered="false" :dis-hover="true" v-for="(upper,index) in upperList" :key="upper.id">
+                <template slot="title"> 第{{index + 1}}题</template>
+                <Scroll :height="300">
+                    {{upper.upperContent}}
+                </Scroll>
             </Card>
         </Drawer>
     </div>
@@ -15,7 +22,7 @@
 
 <script>
 
-    import { readStudentUppers } from "@/api/academic/exam";
+    import { readStudentUppers,endExam } from "@/api/academic/exam";
 
     export default {
         name: "read-upper-exam",
@@ -23,30 +30,60 @@
             return {
                 value : false,
                 examData:{},
-                data:[],
-                loading:true
+                upperList:[],
+                studentExamContent :[]
             }
         },
         watch:{
             value(data){
                 if(data){
-                    readStudentUppers(this.examData).then(res=>this.data = res.data.data);
-                }
-            },
-            data(data){
-                if(data && data.length > -1){
-                    this.loading = false;
+                    readStudentUppers(this.examData).then(res=>{
+                        console.log(res.data.data);
+                        this.upperList = res.data.data.uppers;
+                        this.studentExamContent = res.data.data.list;
+                    });
                 }
             }
         },
         computed:{
             columns(){
                 return [
-
+                    {type:'index',title:'序号',align:'center'},
+                    {type:'nickName',title:'学生姓名',align:'center',render(h,params){
+                        return h('span',{},params.row.user.nickName);
+                    }},
+                    {type:'opt',title:'视频',align:'center',render(h,params){
+                        return h('Button',{props:{type:'error',size:'small',ghost:true,icon:'ios-videocam',disabled:params.row.list[0] == null},on:{click(){
+                            alert("出视频界面");
+                        }}},'观看');
+                    }},
+                    {type:'upperScore',title:'上机题评分',align:'center',render(h,params){
+                        return h('span',{},params.row.score.upperScore);
+                    }}
                 ];
             },
             computed_table_data(){
+                return this.studentExamContent;
+            }
+        },
+        methods:{
+            handleEndExam(data){
+                this.$Modal.confirm({
+                    title:'友情提示',
+                    content:'确定要结束本次考试吗?',
+                    onOk:()=>{
+                        endExam(this.examData).then(res=>{
+                            if(res.data.code === 10000){
+                                this.$Message.success(res.data.message);
+                                this.value = false;
+                                this.$parent.getExamList(this.$parent.params);
+                            }else{
+                                this.$Message.error(res.data.message);
+                            }
 
+                        })
+                    }
+                });
             }
         }
     }
